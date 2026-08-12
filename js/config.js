137665a5a8fb4868cbb8b58f1ce5282ce5d4b057
sivamani-demos <sivamanishetty@gmail.com>
2026-08-12 23:39:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
 
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbzhoRIaJnjVTkobAi-FiwEn1lBJ4QlvaYXyb3sFO9eeFPO4EVO786J9WhjlVFd4N0iFGw/exec",
+  CACHE_KEY: "hostel_store_cache",
+  TOTAL_BEDS: 79
+};
